4c488aa876395b96d00ab51a3f777efd89cab8c4
CodeofRahul <420kumarahul@gmail.com>
2024-12-21 14:54:58 +0530

    app_blueprint with simple <h1> html tag

--- app_blueprint.py
+++ app_blueprint.py
@@ -4,12 +4,12 @@ app_blueprint = Blueprint('app_blueprint', __name__)
 
 @app_blueprint.route('/')
 def index():
-    return "Home page"
+    return "<h1>Home page</h1>"
 
 @app_blueprint.route('/about')
 def about():
-    return "About"
+    return "<h1>About</h1>"
 
 @app_blueprint.route('/contact')
 def contact():
-    return "Contact Us"
+    return "<h1>Contact Us</h1>"
