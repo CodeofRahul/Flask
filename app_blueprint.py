5e52c5cbb5fd26717a13edf79874458412a5d38e
CodeofRahul <420kumarahul@gmail.com>
2024-12-21 16:38:13 +0530

    image added in app_blueprint.py and added in index.html using for loop

--- app_blueprint.py
+++ app_blueprint.py
@@ -2,9 +2,16 @@ from flask import Blueprint, render_template
 
 app_blueprint = Blueprint('app_blueprint', __name__)
 
+# variables
+popular_products = [
+    {'name':'Rahul T-shirt', 'size':'small', 'price':5, 'img': '/images/red.jfif'},
+    {'name':'Rahul T-shirt', 'size':'medium', 'price':10, 'img': '/images/red.jfif'},
+    {'name':'Rahul T-shirt', 'size':'large', 'price':15, 'img': '/images/red.jfif'}
+]
+
 @app_blueprint.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", popular_products=popular_products)
 
 @app_blueprint.route('/about')
 def about():
